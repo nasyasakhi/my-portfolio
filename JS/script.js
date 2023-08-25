29bb0635eb25ec1d733dbb3f5d852b4cfb2dacb8
nasyasakhi <nasyasakhi6@gmail.com>
2023-08-25 14:05:31 +0700

    first commit

---
+++ JS/script.js
@@ -0,0 +1,15 @@
+let menuIcon = document.querySelector('#menu-icon')
+let navbar = document.querySelector('.navbar')
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x')
+    navbar.classList.toggle('active')
+}
+
+
+window.onscroll = () => {
+    // let header = document.querySelector('header')
+
+    menuIcon.classList.remove('bx-x')
+    navbar.classList.remove('active')
+}
